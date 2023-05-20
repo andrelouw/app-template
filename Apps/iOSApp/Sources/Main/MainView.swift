87fb4bac40c93953b649320474d3ead7657df4f3
Andre Louw <andrelouw@hey.com>
2023-05-20 16:55:46 +0200

    Add MainView as home view to app

---
+++ Apps/iOSApp/Sources/Main/MainView.swift
@@ -0,0 +1,34 @@
+import SwiftUI
+
+struct MainView: View {
+  var body: some View {
+    ZStack {
+      MyAppAsset.brandPrimary.swiftUIColor
+        .ignoresSafeArea()
+      VStack {
+        Text("Welcome to")
+          .font(.headline)
+          .foregroundColor(MyAppAsset.fontSecondary.swiftUIColor)
+        Text(Bundle.main.displayName ?? "App")
+          .font(.largeTitle)
+          .foregroundColor(MyAppAsset.fontPrimary.swiftUIColor)
+      }
+    }
+  }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
+
+// MARK: - Private extensions -
+
+// MARK: - Bundle
+private extension Bundle {
+  // Name of the app - title under the icon.
+  var displayName: String? {
+    return object(forInfoDictionaryKey: "CFBundleDisplayName") as? String ?? object(forInfoDictionaryKey: "CFBundleName") as? String
+  }
+}
--- Apps/iOSApp/Sources/Main/MainViewController.swift
+++ Apps/iOSApp/Sources/Main/MainViewController.swift
@@ -1,8 +1,13 @@
 import UIKit
+import SwiftUI
 
-final class MainViewController: UIViewController {
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    view.backgroundColor = MyAppAsset.accentColor.color
+final class MainViewController: UIHostingController<MainView> {
+  init() {
+    super.init(rootView: MainView())
+  }
+
+  @available(*, unavailable)
+  required dynamic init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
   }
 }
