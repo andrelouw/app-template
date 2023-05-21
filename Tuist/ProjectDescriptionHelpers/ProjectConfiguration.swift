36351d36677a4282ba2336766d47ad45a2825b62
Andre Louw <andrelouw@hey.com>
2023-05-21 09:55:07 +0200

    Add min deployment target for ios and macos to tuist generation
    Fixes #1

--- Tuist/ProjectDescriptionHelpers/ProjectConfiguration.swift
+++ Tuist/ProjectDescriptionHelpers/ProjectConfiguration.swift
@@ -1,6 +1,9 @@
 import ProjectDescription
 
 public struct ProjectConfiguration {
+  public static let organizationName = "andrelouw"
   public static let bundleID = "com.andrelouw"
   public static let appName = "MyApp"
+  public static let iOSVersion = "16.4"
+  public static let macOSVersion = "13.3"
 }
--- Tuist/ProjectDescriptionHelpers/Settings+SettingsDictionary.swift
+++ Tuist/ProjectDescriptionHelpers/Settings+SettingsDictionary.swift
@@ -7,9 +7,11 @@ extension Settings {
 }
 
 extension SettingsDictionary {
+  // TODO: Add local signing for mac apps
   public static func base(for platforms: [Platform]) -> Self {
     .init()
     .supportedPlatforms(platforms)
+    .deploymentTargets(platforms)
   }
 
   private func supportedPlatforms(_ platforms: [Platform]) -> SettingsDictionary {
@@ -28,5 +30,19 @@ extension SettingsDictionary {
     }
 
     return merging(["SUPPORTED_PLATFORMS": SettingValue(stringLiteral: supportedPlatforms.joined(separator: " "))])
+  }
+
+  func deploymentTargets(_ platforms: [Platform]) -> SettingsDictionary {
+    var supportedPlatforms = [String: SettingValue]()
+
+    if platforms.contains(.iOS) {
+      supportedPlatforms["IPHONEOS_DEPLOYMENT_TARGET"] = SettingValue(stringLiteral: ProjectConfiguration.iOSVersion)
+    }
+
+    if platforms.contains(.macOS) {
+      supportedPlatforms["MACOSX_DEPLOYMENT_TARGET"] = SettingValue(stringLiteral: ProjectConfiguration.macOSVersion)
+    }
+
+    return merging(supportedPlatforms)
   }
 }
