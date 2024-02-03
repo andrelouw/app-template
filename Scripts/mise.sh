58cf87cab0acf7679aa3a012e32e945fe448f950
André Louw <andrelouw@squareup.com>
2024-02-03 16:20:59 +0100

    Add mise installation and move xcodes and tuist to be managed by mise

---
+++ Scripts/mise.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Install mise to ~/.local/bin/mise
+curl https://mise.run | sh
+# Ensure mise is activated everytime a new shell session starts
+echo 'eval "$(~/.local/bin/mise activate zsh)"' >>"${ZDOTDIR-$HOME}/.zshrc"
+# Activate mise for this script session
+~/.local/bin/mise activate --shims
+# Install (upgrade) all missing tools in .mise.toml
+~/.local/bin/mise up
